574bf8b51f3d5343a9a9813dd1ca96e189f52489
Ryu K <ryuuwiz@gmail.com>
2026-09-17 21:36:29 +0700

    fix: https enforce

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,14 +20,15 @@ use App\Observers\ProfilBandwidthObserver;
 use App\Services\Whatsapp\WhatsappClient;
 use Carbon\CarbonImmutable;
 use Illuminate\Auth\Events\Login;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 use Livewire\Livewire;
@@ -55,6 +56,10 @@ class AppServiceProvider extends ServiceProvider
         $this->registerEventListeners();
         $this->registerModelObservers();
         $this->configureSuperAdminGate();
+
+        if (app()->environment('production') || env('FORCE_HTTPS', false)) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
